daa813a9ad6a0b17e2024ce5a1481464395e5dbb
jeansamist <jeansamist@gmail.com>
2025-03-03 16:18:25 +0100

    fix rasengan config

--- rasengan.config.js
+++ rasengan.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'rasengan';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "rasengan";
 
-<<<<<<< HEAD
-export default defineConfig(async () => {
-  return {
-    vite: {
-      plugins: [tailwindcss()],
-    },
-  };
-=======
 export default defineConfig({
   reactStrictMode: true,
   server: {
     host: "0.0.0.0",
   },
->>>>>>> 464aa4626ae71763fdf2f4cbe943f1745524aa6f
 });
