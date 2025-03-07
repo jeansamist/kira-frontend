2d8cd580804d4256b9e60d285c571bbea3676b25
jeansamist <jeansamist@gmail.com>
2025-03-07 17:21:53 +0100

    feat: add @rasenganjs/vercel dependency and integrate with rasengan config

--- rasengan.config.js
+++ rasengan.config.js
@@ -1,10 +1,12 @@
+import { configure } from "@rasenganjs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "rasengan";
+import { rasengan } from "rasengan/plugin";
 
 export default defineConfig(async () => {
   return {
     vite: {
-      plugins: [tailwindcss()],
+      plugins: [tailwindcss(), rasengan({ adapter: configure() })],
     },
     reactStrictMode: true,
     server: {
